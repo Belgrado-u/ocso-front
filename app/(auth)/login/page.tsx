e2ba7d282c286d374b726f0d48477f4ed6add0e4
Belgrado-u <cheo2931@gmail.com>
2024-10-19 20:13:09 -0600

    65-Fetch

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,21 +1,30 @@
+import { API_URL } from "@/constants";
 import { Button,Input } from "@nextui-org/react";
+import axios from "axios";
 import Link from "next/link";
 
+
 export default function LoginPage(){
+    const handleSubmit = async(e:React.FormEvent) =>{
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const {data} = await axios.post(`${API_URL}/auth/login`,formData)
+        console.log(data)
+        return;
+    }
     return (
-        <div className="bg-orange-500 px-10 py-2 rounded-md">
+        <form className="bg-orange-500 px-10 py-2 rounded-md">
             <p className="text-2xl my-4 text-white">Iniciar Sesion</p>
             <div className="flex flex-col gap-2 my-4 items-center">
-            <Input label="Email" type="email" isRequired={true} size="sm"/>
-            <Input label="Contraseña" type="password" isRequired={true} size="sm"/>
-            
-        </div>
+                <Input label="Email" name="userEmail" type="email" isRequired={true} size="sm"/>
+                <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm"/>
+            </div>
             <div className="flex flex-col items-center gap-2">
-                <Button color="primary">Registrarse</Button>
+                <Button color="primary">Iniciar Sesion</Button>
                 <p className="text-white">
                     ¿No tienes cuenta? <Link href="/signup" className="text-red-600 underline"> Registrate </Link>
                 </p>
             </div>
-        </div>
+        </form>
     )
 }
