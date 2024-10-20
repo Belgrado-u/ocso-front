a51d2d947f2be06b70fc4d9c0d68e11a8c8c751a
Belgrado-u <cheo2931@gmail.com>
2024-10-19 22:28:46 -0600

    feat:cookies in SSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,33 @@
 'use client';
 import { API_URL } from "@/constants";
-import { Button,Input } from "@nextui-org/react";
+import { Button,Input, Spinner } from "@nextui-org/react";
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 
 export default function LoginPage(){
+    const [submitting,setSubmitting]=useState(false)
+    const router=useRouter()
     const handleSubmit = async(e:React.FormEvent) =>{
+        setSubmitting(true),
         e.preventDefault()
         const formData = new FormData(e.target);
         let authData: any ={}
         authData.userEmail=formData.get("userEmail");
         authData.userPassword=formData.get("userPassword");
-        const {data} = await axios.post(`${API_URL}/auth/login`,{
-            ...authData
-        },{
-            withCredentials:true,
-        });
+        try{
+            const response = await axios.post(`${API_URL}/auth/login`,{
+                ...authData
+            },{
+                withCredentials:true,
+            });
+            if(response.status == 201) router.push('/dashboard');
+            setSubmitting(false)
+        } catch(e){
+            setSubmitting(false);
+        }
         return;
     }
     return (
@@ -27,7 +38,12 @@ export default function LoginPage(){
                 <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm"/>
             </div>
             <div className="flex flex-col items-center gap-2">
-                <Button color="primary" type="submit">Iniciar Sesion</Button>
+                <Button 
+                    color="primary" 
+                    type="submit" 
+                    disabled={submitting}>
+                    {submitting ? "Enviando..." : "Iniciar Sesion"}
+                </Button>
                 <p className="text-white">
                     ¿No tienes cuenta? <Link href="/signup" className="text-red-600 underline"> Registrate </Link>
                 </p>
