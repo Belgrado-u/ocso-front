8f9311d9f0be33315f92ed929286085e5e1f5cd3
Belgrado-u <cheo2931@gmail.com>
2024-10-26 17:50:06 -0600

    feat:Cards for employees location

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -4,21 +4,37 @@ import {TOKEN_NAME} from "@/constants"
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
 
-const CountPage =async () => {
+const LocationsPage =async ({searchParams}: { 
+    searchParams:{ [key: string]: string | string[] | undefined };
+}) => {
     const userCookies= cookies();
     const token=userCookies.get(TOKEN_NAME)?.value;
-    const  {data}=await axios.get<Location[]>(
+    let  {data}=await axios.get<Location[]>(
         "http://127.0.0.1:4000/locations",
         {
             headers:{
                 Authorization:`Bearer ${token}`,
+            },
         },
-    },
-);
+    );
+    data=[
+        {
+            locationId:0,
+            locationName:"Ninguna",
+            locationLatLng: [0,0],
+            locationAdress:"No existe"
+        },
+        ...data
+    ]
+
     return (
-    <div className="w-2/12">
-        <SelectLocation locations={data}/>
-    </div>
+        <div className="w-8/12">
+            <div className="w-full flex  flex-col items-center h-[90vh] bg-red-50">
+                <div className=" w-1/2 my-10">
+                    <SelectLocation locations ={data} store={searchParams?.store}/>
+                </div>
+            </div>
+         </div>
     );
 };
-export default CountPage;
+export default LocationsPage;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,5 +17,5 @@ export default function LayoutDashboard({
                 {locations}
             </div>
         </div>
-    )
+    );
 }
