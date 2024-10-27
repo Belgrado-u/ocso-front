906333a769b36873b8d293c7679e1c72a4b3c546
Belgrado-u <cheo2931@gmail.com>
2024-10-26 18:52:32 -0600

    Location Card

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import {TOKEN_NAME} from "@/constants"
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
+import LocationCard from "./_components/LocationCard";
 
 const LocationsPage =async ({searchParams}: { 
     searchParams:{ [key: string]: string | string[] | undefined };
@@ -32,6 +33,10 @@ const LocationsPage =async ({searchParams}: {
             <div className="w-full flex  flex-col items-center h-[90vh] bg-red-50">
                 <div className=" w-1/2 my-10">
                     <SelectLocation locations ={data} store={searchParams?.store}/>
+                </div>
+
+                <div className=" w-8/12">
+                    <LocationCard store ={searchParams.store}/>
                 </div>
             </div>
          </div>
