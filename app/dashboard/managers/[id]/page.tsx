e9b0ea2706d6923a9f230e3590cd7acce34a1fba
Belgrado-u <cheo2931@gmail.com>
2024-11-02 18:55:03 -0600

    feat:New manager route config

--- app/dashboard/managers/[id]/page.tsx
+++ app/dashboard/managers/[id]/page.tsx
@@ -2,6 +2,7 @@ import { API_URL } from "@/constants";
 import { Manager } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import { Card, CardBody, CardHeader,Divider} from "@nextui-org/react";
+import ManagerCard from "./_components/ManagerCard";
 
 export default async function ManagerPage({
     params,
@@ -20,37 +21,8 @@ export default async function ManagerPage({
      });
     const data: Manager= await response.json();
     return (
-            <Card className="mx-20 py-2 bg-orange-50">
-                <CardHeader>
-                <p className="w-full">
-                    Nombre: <b>{data.managerFullName}</b>
-                </p>
-                </CardHeader>
-                <Divider/>
-                <CardBody>
-                    <p className="w-full">
-                        Email: <b>{data.managerEmail}</b>
-                    </p>
-                    <p className="w-full">
-                        Telefono: <b>{data.managerPhoneNumber}</b>
-                    </p>
-                    {
-                    data.location?(
-                        <>
-                        <p>
-                        Tienda:<b>{data.location.locationName}</b></p>
-                    <iframe 
-                        className="border-2 border-orange-800 rounded-md my-2"
-                        width="300"
-                        height="200"
-                        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAz0y6dhhUVleZmt7-H4PO1QQWCSEz3LBg
-                        &q=${data.location.locationLatLng[0]},${data.location.locationLatLng[1]}`}
-                    ></iframe>
-                        </>
-                    ): (
-                    <p>No tiene tienda</p>)
-                }
-                </CardBody>
-            </Card>
+           <div>
+            <ManagerCard manager={data}/>
+           </div>
     );
 }
