066078d90cced7f47df6de5be937d1d37ae644cf
Belgrado-u <cheo2931@gmail.com>
2024-11-02 22:48:07 -0600

    Pagina de Provedores

---
+++ app/dashboard/providers/_components/ProviderCard.tsx
@@ -0,0 +1,25 @@
+import { Provider } from "@/entities";
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+
+export default function ProviderCard ({provider}:{provider: Provider}){
+    return(
+        <Card>
+            <CardHeader>{provider.providerName}</CardHeader>
+            <Divider/>
+            <CardBody>
+                <p>Correo electronico:</p>
+                <b>{provider.providerEmail}</b>
+                <p>Numero de Telefono:</p>
+                <b>{provider.providerPhoneNumber}</b>
+                {
+                    provider.products?(
+                        <p>
+                            Tiene <b>{provider.products.length}</b> productos
+                        </p>
+                    ):(
+                    <p>No tiene productos</p>
+                )}
+            </CardBody>
+        </Card>
+    );
+}
