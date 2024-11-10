3cde8fef95b7beab060292c18dc9b1d9670d6da9
Belgrado-u <cheo2931@gmail.com>
2024-11-09 18:51:14 -0600

    feat:ProvidersFinish

--- app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
+++ app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
@@ -1,6 +1,8 @@
 import updateProvider from "@/actions/providers/update";
 import { Provider } from "@/entities";
 import { Button, Input } from "@nextui-org/react";
+import DeleteProvider from "./DeleteProvider";
+import DeleteProviderButton from "./DeleteButton";
 
 export default function FormUpdateProvider ({provider} : {provider:Provider}) {
     const {providerId} =provider;
@@ -12,6 +14,10 @@ export default function FormUpdateProvider ({provider} : {provider:Provider}) {
             <Input className="max-w-[250px]" defaultValue={provider.providerEmail} label="Correo" placeholder="bussines@pecsi.com" name="providerEmail"/>
             <Input className="max-w-[250px]" defaultValue={provider.providerPhoneNumber} label="Numero" placeholder="444XXXXXXX" name="providerPhoneNumber"/>
                 <Button color="primary" type="submit">Actualizar</Button>
+                <DeleteProvider>
+                    <h1 className="text-white text-4xl text-center">¿Estas seguro de eliminar al provedor <b>{provider.providerName}</b>?</h1>
+                    <DeleteProviderButton providerId={providerId}/>
+                </DeleteProvider>
         </form>
         </>
     )
